027d84090d48d1f265195ab5e29b591ec4b911fb
swagar <you@example.com>
2023-10-06 10:09:46 +0530

    ee

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-north-1"  # Change this to your desired region
+  region = "us-east-1"  # Change this to your desired region
   access_key  = var.aws_access_key
   secret_key  = var.aws_secret_key
 }
