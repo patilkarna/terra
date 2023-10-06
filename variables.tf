37cee1e097cd346b3118ea4551b14b3fa3cb5876
patilkarna <140930898+patilkarna@users.noreply.github.com>
2023-10-06 10:32:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_access_key" {
   description = "AWS Access Key ID"
-  default     = "AKIA5QYHS3IMAHTHIEPS"
+  default     = 
 
 }
 
 variable "aws_secret_key" {
   description = "AWS Secret Access Key"
-  default     = "EaE92I0sLzRxrRKu++50dbRUYvz/GcQjiPfhDA1K"
+  default     = 
 }
 
