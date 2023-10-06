8e812f5874684330bcbf3f37241638b0661089ec
swagar <you@example.com>
2023-10-06 10:03:04 +0530

    ss

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_access_key" {
   description = "AWS Access Key ID"
-  default     = "AKIA5QYHS3IMMKG37SHV"
+  default     = "AKIA5QYHS3IMNMXWJ4V6"
 
 }
 
 variable "aws_secret_key" {
   description = "AWS Secret Access Key"
-  default     = "Jkx5eD4I/bEX7dCn6MewL1T8BBIHEuD1CzLNiH1D0"
+  default     = "KNSIUybe+8FQabfXDriDMtbL3N4UF/+fESXUgzqo"
 }
 
