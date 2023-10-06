b1127e95f918b3d93830074e3f87218f0546b2ef
swagar <you@example.com>
2023-10-06 10:11:42 +0530

    ss

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example_instance" {
-  ami           = "ami-08766f81ab52792ce"  # Ubuntu 20.04 LTS (HVM)
+  ami           = "ami-053b0d53c279acc90"  # Ubuntu 20.04 LTS (HVM)
   instance_type = "t2.micro"
 }
 
