049b6575306d77a98bff50bc295463e4076f97c7
swagar <you@example.com>
2023-10-06 09:20:06 +0530

    www

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,5 @@
-output "printname" {
-  value = "hello ${var.username} your age is ${var.age}"
+resource "aws_instance" "example_instance" {
+  ami           = "ami-08766f81ab52792ce"  # Ubuntu 20.04 LTS (HVM)
+  instance_type = "t2.micro"
 }
+
