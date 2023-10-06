b27c89e3ebd4a5128dc0a3c6215e1de0cd93679a
swagar <you@example.com>
2023-10-06 13:11:26 +0530

    xx

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,9 @@
-resource "aws_instance" "example_instance" {
-  ami           = "ami-053b0d53c279acc90"  # Ubuntu 20.04 LTS (HVM)
-  instance_type = "t2.micro"
-}
+# resource "aws_instance" "example_instance" {
+#   ami           = "ami-053b0d53c279acc90"  # Ubuntu 20.04 LTS (HVM)
+#   instance_type = "t2.micro"
+# }
 
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "terraform-s3-bucket"
+  acl    = "private"  # You can adjust the ACL as needed
+}
