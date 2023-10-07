5c5b28d3f06a42ca577e0983d74a4c8a7ce4cf32
swagar <you@example.com>
2023-10-07 16:11:25 +0530

    aa

--- 3_tier/jupiter-website-ecs/backend.tf
+++ 3_tier/jupiter-website-ecs/backend.tf
@@ -1,7 +1,7 @@
 # store the terraform state file in s3
 terraform {
   backend "s3" {
-    bucket    = history-log-backupI
+    bucket    = "history-log-backup"
     key       = "teeraform-state.tfstae"
     region    = "us-east-1"
     profile   = "default"
